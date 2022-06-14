3af2d6e1600fccaa996ea932f721d4e3cc26cae1
LUKELB89 <lukelbcoding89@gmail.com>
2022-06-14 01:07:58 -0300

    added

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-const pokemonCount = 151;
+const pokemonCount = 251;
 var pokedex = {}; // {1 :"name" : "bulbasaur", "img" : url, "type" : ["grass", "poison"], "desc" : "...."}
 
-window.onload = function() {
-	getPokemon(1);
+window.onload = async function() {
+	//getPokemon(1);
+	for(let i = 1; i <= pokemonCount; i++) {
+		await getPokemon(i);
+		//<div id="1" class="pokemon-name">BULBASAUR</div>		
+		let pokemon = document.createElement("div");
+		pokemon.id = i;
+		pokemon.innerText = i.toString() + ". " + pokedex[i]["name"].toUpperCase();
+		pokemon.classList.add("pokemon-name");
+		pokemon.addEventListener("click", updatePokemon);
+		document.getElementById("pokemon-list").append(pokemon);
+	}
+
+	console.log(pokedex);
 }
 
 async function getPokemon(num) {
@@ -10,6 +22,37 @@ async function getPokemon(num) {
 
 	let response = await fetch(url);
 	let pokemon = await response.json();
-	console.log(pokemon)
+	//console.log(pokemon)
 
+	let pokemonName = pokemon["name"];
+	let pokemonType =pokemon["types"];
+	let pokemonImg = pokemon["sprites"]["front_default"];
+
+	res = await fetch(pokemon["species"]["url"]);
+	let pokemonDesc = await res.json()
+
+	//console.log(pokemonDesc);
+	pokemonDesc = pokemonDesc["flavor_text_entries"][7]["flavor_text"]
+
+	pokedex[num] = {"name" : pokemonName, "img" : pokemonImg, "types" : pokemonType, "desc" : pokemonDesc}
+}
+
+function updatePokemon() {
+	document.getElementById("pokemon-img").src = pokedex[this.id]["img"];
+
+	//clear previous type
+	let typesDiv = document.getElementById("pokemon-types");
+	while (typesDiv.firstChild) {
+		typesDiv.firstChild.remove();
+	}
+
+	//update types
+		let types = pokedex[this.id]["types"];
+		for (let i = 0; i < types.length; i++) {
+			let type = document.createElement("span");
+			type.innerText = types[i]["type"]["name"].toUpperCase();
+			type.classList.add("type-box");
+			type.classList.add(types[i]["type"]["name"]);
+			typesDiv.append(type);
+	}
 }
